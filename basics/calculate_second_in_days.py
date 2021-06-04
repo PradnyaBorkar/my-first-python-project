73545a40e94dbcaa9c831b629a60f00d2cc60cdd
PradnyaBorkar <pradnya.borkar@springernature.com>
2021-06-04 12:28:22 +0530

    Add validations

--- basics/calculate_second_in_days.py
+++ basics/calculate_second_in_days.py
@@ -6,6 +6,18 @@ def from_days_to_seconds(number_of_days):
     return f"{number_of_days} days are {calculation_units * number_of_days} {name_of_units}"
 
 
+def validate_and_execute():
+    if user_input.isdigit():
+        number_of_days = int(user_input)
+        if number_of_days > 0:
+            return from_days_to_seconds(int(user_input))
+        elif number_of_days == 0:
+            return "Please enter value greater than 0"
+        else:
+            return "Please enter a positive value"
+    else:
+        return "Please enter numeric value"
+
+
 user_input = input("please enter number of days to convert \n")
-seconds = from_days_to_seconds(int(user_input))
-print(f"{seconds}")
+print(validate_and_execute())
